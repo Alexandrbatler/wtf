3791175fba323e168f4c60d57e5b40d28832d16f
Maria Nurgalieva <klookvaroom@gmail.com>
2017-02-27 14:59:13 +0500

    + Добавлена главная страница
    Добавлена главная страница сайта и заполнен <head>

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+
+    <title>WTF? - What the Film?</title>
+
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+
+<header></header>
+<section></section>
+<footer></footer>
+
+</body>
+</html>
