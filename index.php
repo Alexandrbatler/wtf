9eb10d3fd5dfb4554d1438afe44c810e65eb2539
FominRA <lehman-08@yandex.ru>
2017-02-27 15:25:03 +0500

    Верстка главной страницы

--- index.php
+++ index.php
@@ -9,9 +9,75 @@
 </head>
 <body>
 
-<header></header>
-<section></section>
-<footer></footer>
+<div class="site__container">
+    <header class="header__container">
+        <div class="row container">
+            <div class="col-xs-4">
+                <h2 class="header__logo">What The Film?!</h2>
+            </div>
+            <div class="col-xs-8">
+                <a href="#" class="header__auth">Login</a>
+            </div>
+        </div>
+    </header>
+
+    <section class="main__container">
+        <div class="main__bg"></div>
+        <div class="row container">
+            <div class="col-xs-8 main__description">
+                <header class="main__title">
+                    <h2 class="main__heading">WTF?!</h2>
+                    <p class="main__subheading">What the Film?!</p>
+                </header>
+                <hr class="main__hr">
+                <section class="main__description">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, alias esse fugit impedit in
+                    libero magnam minus nulla odit perferendis provident tempora voluptate. Aliquid doloremque, et eum
+                    excepturi in quasi quisquam soluta vel. A dicta doloremque dolores excepturi nisi perspiciatis
+                    placeat. Aut dicta dignissimos doloribus dolorum error facilis hic labore, libero magni molestias
+                    natus officia placeat reiciendis! Aperiam at beatae commodi cupiditate deserunt dignissimos dolores
+                    doloribus eos error et exercitationem expedita explicabo illum impedit iste laboriosam magnam magni
+                    minima modi molestiae natus non nostrum praesentium, quae quam quis quod recusandae rerum sed sequi
+                    suscipit tempore temporibus ullam. Amet ea eius labore laborum minus neque quas, rerum vel
+                    voluptates. A ad adipisci autem cum cumque dicta doloremque eos error esse eum facilis, illo
+                    incidunt ipsam iure magnam molestiae molestias nostrum numquam odio omnis quaerat quam rem
+                    repudiandae sit temporibus ullam unde! Aperiam, aut sequi! Ad adipisci inventore odio quas quasi
+                    vitae.
+                </section>
+                <div class="main__start">
+                    <a href="#" class="btn btn-warning main__start-btn">Start the Game!</a>
+                </div>
+            </div>
+
+            <div class="col-xs-3 col-xs-offset-1 main__reg">
+                <form method="post" class="main__reg-form">
+                    <h3>Registry</h3>
+                    <div class="form-group">
+                        <input type="text" name="login" class="form-control" placeholder="Login">
+                    </div>
+                    <div class="form-group">
+                        <input type="password" name="password" class="form-control" placeholder="Password">
+                    </div>
+                    <button type="submit" class="btn btn-primary btn-block">Registry</button>
+                    <hr class="main__hr">
+                    <p>or</p>
+                    <a href="#" class="btn btn-success btn-block">Login</a>
+                </form>
+            </div>
+        </div>
+    </section>
+
+    <footer class="footer__container">
+        <div class="row container">
+            <div class="col-xs-10">
+                <a href="#" class="footer__rating">Rating</a>
+            </div>
+            <div class="col-xs-2 footer__copyright">
+                wtf.local © 2017
+            </div>
+        </div>
+    </footer>
+</div>
 
 </body>
 </html>
